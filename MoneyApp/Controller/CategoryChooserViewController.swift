5137ae51e8718983ecf810355e85939023868736
Vlad Chabaniuk <vladorackle@gmail.com>
2019-08-21 22:01:03 +0800

    editable category list refinements

--- MoneyApp/Controller/CategoryChooserViewController.swift
+++ MoneyApp/Controller/CategoryChooserViewController.swift
@@ -61,19 +61,15 @@ class CategoryListViewController: UITableViewController {
         selectedCategory = wallet.categoryList.listOfAllCategories[indexPath.row]
         return indexPath
     }
-    
 
     
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
         let category = wallet.categoryList.listOfAllCategories[indexPath.row]
         if category.canBeDeleted! {
-            return true
+            return UITableViewCell.EditingStyle.delete
         }
-        return false
+        return UITableViewCell.EditingStyle.none
     }
-    
-
     
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
@@ -90,19 +86,8 @@ class CategoryListViewController: UITableViewController {
     
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+        wallet.categoryList.moveCategory(from: fromIndexPath.row, to: to.row)
     }
-    
-   
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    
     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
